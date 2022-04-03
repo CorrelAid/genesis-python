a9bbd18659f2c02afd6a1bcc7088eadf36d36430
Michael Aydinbas <michael.aydinbas@gmail.com>
2022-04-03 15:45:17 +0200

    fix pylint errors

--- src/pygenesis/__init__.py
+++ src/pygenesis/__init__.py
@@ -1,1 +1,15 @@
+"""
+GENESIS Python wrapper
+~~~~~~~~~~~~~~~~~~~~~
+pygenesis is a Python wrapper for the GENESIS web service interface (API).
+
+Basic usage:
+```python
+   >>> import pygenesis as pgen
+   >>> print("Version:", pgen.__version__)
+```
+
+:copyright: (c) 2022 by CorrelAid.
+:license: tbd
+"""
 __version__ = "0.1.0"
