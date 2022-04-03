972e10cf8747542c99fe94d59dbdda6f5db7d616
Michael Aydinbas <michael.aydinbas@gmail.com>
2022-04-03 20:16:36 +0200

    add flake8 config to pyproject.toml, add flake8-docstrings plugin to repo, add .pylintrc config from Google

--- src/pygenesis/__init__.py
+++ src/pygenesis/__init__.py
@@ -1,15 +1,10 @@
-"""
-GENESIS Python wrapper
-~~~~~~~~~~~~~~~~~~~~~
-pygenesis is a Python wrapper for the GENESIS web service interface (API).
+"""pygenesis is a Python wrapper for the GENESIS web service interface (API).
 
 Basic usage:
-```python
-   >>> import pygenesis as pgen
-   >>> print("Version:", pgen.__version__)
-```
 
-:copyright: (c) 2022 by CorrelAid.
-:license: tbd
+```python
+import pygenesis as pgen
+print("Version:", pgen.__version__)
+```
 """
 __version__ = "0.1.0"
