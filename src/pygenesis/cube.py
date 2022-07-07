2b9e30ab5a25ae172175cb58b47f22bbe39df51e
Michael Aydinbas <michael.aydinbas@gmail.com>
2022-07-07 22:04:41 +0200

    activate more rules

--- src/pygenesis/cube.py
+++ src/pygenesis/cube.py
@@ -1,20 +1,47 @@
-"""Module provides functionality to parse a cube data as returned by the /data/cubefile endpoint of GENESIS."""
+"""Module provides functionality to parse cubefile data provided by GENESIS."""
 import copy
 
 import pandas as pd
 
 
 def is_cube_metadata_header(line: str) -> bool:
+    """Check if a line is a cube metadata header.
+
+    Args:
+        line (str): A single line of a cubefile.
+
+    Returns:
+        bool: True if the line starts with a "K", False otherwise.
+    """
     return line[0] == "K"
 
 
 def get_cube_metadata_header_type(line: str) -> str:
+    """Return the header type.
+
+    Args:
+        line (str): A single line of a cubefile.
+
+    Returns:
+        str: The header type, which is the second entry in the header.
+    """
     return line.split(";")[1]
 
 
 def get_cube_metadata_header(
     line: str, rename_duplicates: bool = False
 ) -> list[str]:
+    """Return the metadata header of a cubefile.
+
+    Args:
+        line (str): A single line of a cubefile.
+        rename_duplicates (bool, optional): If False, the raw header is returned.
+            If True, identical column names are appended with a unique counter.
+            Defaults to False.
+
+    Returns:
+        list[str]: A list of column names, except for "nur Werte" and "mit Werten".
+    """
     raw_header = line.split(";")[2:]
     raw_header = [
         name
@@ -39,16 +66,26 @@ def get_cube_metadata_header(
 
 
 def parse_cube_data_line(line: str) -> list[str]:
+    """Return the content of a cube data line.
+
+    Args:
+        line (str): A single line of a cubefile.
+
+    Returns:
+        list[str]: The content of a cube data line, omitting the first element.
+    """
     return line.split(";")[1:]
 
 
-def parse_cube_metadata_block(
-    rows: list[list[str]], header: list[str]
-) -> pd.DataFrame:
-    return pd.DataFrame(rows, columns=header)
-
-
 def parse_cube(data: str) -> dict:
+    """Main function for parsing a cubefile.
+
+    Args:
+        data (str): The content of a cubefile as returned by GENESIS.
+
+    Returns:
+        dict: A dictionary with each header type as key and the corresponding header block as value.
+    """
     cube = {}
     header = None
     data_block = []
@@ -82,6 +119,20 @@ def rename_axes(
     rename_time_variable: bool = True,
     rename_value_variables: bool = True,
 ) -> dict:
+    """Rename the generic axes of a cubefile with the names found in the metadata.
+
+    Args:
+        cube (dict): A dictionary holding the cube data as returned by `parse_cube()`.
+        rename_classifying_variables (bool, optional): If True, rename classifying variables.
+            Defaults to True.
+        rename_time_variable (bool, optional): If True, rename the time variable.
+            Defaults to True.
+        rename_value_variables (bool, optional): If True, rename the value variables.
+            Defaults to True.
+
+    Returns:
+        dict: _description_
+    """
     cube = copy.deepcopy(cube)
 
     old_cols = []
@@ -103,8 +154,6 @@ def rename_axes(
         )
         new_cols.extend(cube["DQI"]["NAME"].to_list())
 
-    cube["QEI"].rename(
-        columns={old: new for old, new in zip(old_cols, new_cols)}, inplace=True
-    )
+    cube["QEI"].rename(columns=dict(zip(old_cols, new_cols)), inplace=True)
 
     return cube
