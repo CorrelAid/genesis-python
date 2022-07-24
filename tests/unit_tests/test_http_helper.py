bc6a1002a1baec338f1215d4ea9eaf00db9a02b7
frederik9 <frederik@predict42.com>
2022-07-24 13:46:30 +0200

    update unit tests

--- tests/unit_tests/test_http_helper.py
+++ tests/unit_tests/test_http_helper.py
@@ -1,29 +1,29 @@
 import pytest
 
-from src.pygenesis.http_helper import _handle_status_code
+from src.pygenesis.http_helper import _check_invalid_status_code
 
 
-def test__handle_status_code_with_error():
+def test__check_invalid_status_code_with_error():
     """
     Basic tests to check an error status code (4xx, 5xx)
     for _handle_status_code method.
     """
     status_code = 400
     with pytest.raises(Exception) as e:
-        _handle_status_code(status_code)
+        _check_invalid_status_code(status_code)
     assert (
         str(e.value)
         == f"Error {status_code}: The server returned a {status_code} status code"
     )
 
 
-def test__handle_status_code_without_error():
+def test__check_invalid_status_code_without_error():
     """
     Basic test to check a valid status code (2xx)
     for the _handle_status_code method.
     """
     status_code = 200
     try:
-        _handle_status_code(status_code)
+        _check_invalid_status_code(status_code)
     except Exception:
         assert False
