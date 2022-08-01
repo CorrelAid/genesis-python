4b98217b649ca82d6e923cb51f766ce05663d285
Michael Aydinbas <michael.aydinbas@gmail.com>
2022-08-01 18:55:59 +0200

    fix pylint errors; refactor code so that common code for downloading data is now in data.py module and table.py and cube.py only hold specific parsing logic

--- src/pygenesis/table.py
+++ src/pygenesis/table.py
@@ -1,38 +1,16 @@
 """Module contains business logic related to destatis tables."""
 import pandas as pd
 
-from pygenesis.cache import cache_data
 from pygenesis.csv_helper import get_df_from_text
-from pygenesis.http_helper import get_response_from_endpoint
 
 
-@cache_data
-def get_tablefile_data(
-    *, name: str, area: str = "all", **kwargs
-) -> pd.DataFrame:
+def get_tablefile_data(data: str) -> pd.DataFrame:
     """Return table file data as pandas data frame.
 
-    Based on the table name, table area and additional query parameters the
-    tablefile method from the data-endpoint will be queried.
-
     Args:
-        name (str): Name of the table.
-        area (str, optional): Area of the table. Defaults to "all".
+        data (str): Raw tablefile content.
 
     Returns:
         pd.DataFrame: Parsed table file.
     """
-
-    kwargs = kwargs or {}
-
-    params = {
-        "name": name,
-        "area": area,
-        "format": "ffcsv",
-    }
-
-    params |= kwargs
-
-    response = get_response_from_endpoint("data", "tablefile", params)
-
-    return get_df_from_text(response.text)
+    return get_df_from_text(data)
