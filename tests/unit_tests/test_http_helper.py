ca2d0609f845f296d475a4a9a2d818f9a3312127
MarcoHuebner <57489799+MarcoHuebner@users.noreply.github.com>
2022-08-20 11:41:09 +0200

    Added generic response creation for generic http_helper test

--- tests/unit_tests/test_http_helper.py
+++ tests/unit_tests/test_http_helper.py
@@ -1,12 +1,15 @@
+import json
+
 import pytest
+import requests
 
 from src.pygenesis.http_helper import (
-    _check_destatis_status,
     _check_invalid_destatis_status_code,
     _check_invalid_status_code,
 )
 
-# TODO: Add generic dummy request to the server, which is not getting us timed out?
+# TODO: Add generic dummy request to the server, which is not getting us timed out,
+# to test get_response_from_endpoint completely?
 
 
 def test__check_invalid_status_code_with_error():
@@ -35,72 +38,94 @@ def test__check_invalid_status_code_without_error():
         assert False
 
 
-def _status_dict_helper(code: int = 0, type_: str = "Information") -> dict:
-    """ """
-    status_dict = {
-        "Code": code,
-        "Content": "erfolgreich",
-        "Type": type_,
-    }
+def _generic_request_status(
+    status_response: bool = True,
+    code: int = 0,
+    status_type: str = "Information",
+) -> requests.Response:
+    """
+    Helper method which allows to create a generic request.Response that covers all Destatis answers
 
-    # TODO: add raw response
-    """status_dict = {
-        "Ident":{
-            "Service":"find",
-            "Method":"find"
+    Args:
+        status_response (bool): Whether Destatis answer contains a status response
+        code (str): Status response code
+        status_type (str): Status reponse type/ name
+
+    Returns:
+        requests.Response: the response from Destatis
+    """
+    # define possible status dict and texts
+    status_dict = {
+        "Ident": {
+            "Service": "A DESTATIS service",
+            "Method": "A DESTATIS method",
         },
         "Status": {
             "Code": code,
-            "Content": "erfolgreich",
-            "Type": type_,
+            "Content": "Erfolg/ Success/ Some Issue",
+            "Type": status_type,
         },
-    }"""
+    }
 
-    return status_dict
+    response_text = "Some text for a successful response without status..."
+
+    # set up generic requests.Reponse
+    request_status = requests.Response()
+    request_status.status_code = 200  # success
+
+    # TODO: Why is specific (UTF-8) encoding necessary?
+    if status_response:
+
+        request_status._content = json.dumps(status_dict).encode("utf-8")
+    else:
+        request_status._content = response_text.encode("utf-8")
+
+    return request_status
 
 
-# TODO: Is implementation of raw requests response type for actual
-# _check_invalid_destatis_status_code test possible?
 def test__check_invalid_destatis_status_code_with_error():
     """
     Basic tests to check an error status code as defined in the
-    documentation via code (e.g. 104) or name ('Error', 'Fehler').
+    documentation via code (e.g. 104) or type ('Error', 'Fehler').
     """
-    for status_dict in [
-        _status_dict_helper(code=104),
-        _status_dict_helper(type_="Error"),
-        _status_dict_helper(type_="Fehler"),
+    for status in [
+        _generic_request_status(code=104),
+        _generic_request_status(status_type="Error"),
+        _generic_request_status(status_type="Fehler"),
     ]:
-        status_content = status_dict.get("Content")
+        # extract status content which is raised
+        status_content = status.json().get("Status").get("Content")
 
         with pytest.raises(Exception) as e:
-            _check_destatis_status(status_dict)
+            _check_invalid_destatis_status_code(status)
         assert str(e.value) == status_content
 
 
 def test__check_invalid_destatis_status_code_with_warning():
     """
     Basic tests to check a warning status code as defined in the
-    documentation via code (e.g. 22) or name ('Warning', 'Warnung').
+    documentation via code (e.g. 22) or type ('Warning', 'Warnung').
     """
 
-    for status_dict in [
-        _status_dict_helper(code=22),
-        _status_dict_helper(type_="Warnung"),
-        _status_dict_helper(type_="Warning"),
+    for status in [
+        _generic_request_status(code=22),
+        _generic_request_status(status_type="Warnung"),
+        _generic_request_status(status_type="Warning"),
     ]:
         # TODO: Is the best/ most specific way to capture the warning?
         with pytest.warns(UserWarning):
-            _check_destatis_status(status_dict)
+            _check_invalid_destatis_status_code(status)
 
 
 def test__check_invalid_destatis_status_code_without_error():
     """
-    Basic tests to check the successful status code 0 as defined in the documentation.
+    Basic tests to check the successful status code 0 or only text response as defined in the documentation.
     """
-    status_dict = _status_dict_helper()
-
-    try:
-        _check_destatis_status(status_dict)
-    except Exception:
-        assert False
+    for status in [
+        _generic_request_status(),
+        _generic_request_status(status_response=False),
+    ]:
+        try:
+            _check_invalid_destatis_status_code(status)
+        except Exception:
+            assert False
