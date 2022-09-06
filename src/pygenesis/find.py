2026f755420f38dfe623120bd357ad04e8949409
dpleus <danielpleus@hotmail.com>
2022-09-06 20:32:33 +0200

    several improvements based on feedback for PR #66

--- src/pygenesis/find.py
+++ src/pygenesis/find.py
@@ -1,8 +1,10 @@
 """Implements find endpoint to retrieve results based on query"""
 
+import json
+
 import pandas as pd
 
-from pygenesis.http_helper import get_response_from_endpoint
+from pygenesis.http_helper import get_data_from_endpoint
 
 pd.set_option("max_colwidth", None)
 pd.set_option("expand_frame_repr", False)
@@ -14,62 +16,63 @@ class Results:
 
     Attributes:
         df (pd.DataFrame): The DataFrame that contains the data.
-        category (str): Category of the result. E.g. "tables", "cubes".
+        category (str): Category (plural) of the result. E.g. "tables", "cubes".
 
     Methods:
         get_code(): Gets code based on the index of the object.
         get_metadata(): Gets metadata based on the index of the object.
     """
 
-    def __init__(self, result: pd.DataFrame, category: str):
+    def __init__(self, result: pd.DataFrame, category: str) -> None:
         """
         Class that contains the results of a find query.
 
         Args:
-            DataFrame (pd.DataFrame): Result of a search query.
+            result (pd.DataFrame): Result of a search query.
             category (str): Category of the result. E.g. "tables", "cubes"
         """
         self.df = result
         self.category = category
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    def __str__(self) -> int:
         return self.df.to_markdown()
 
-    def __str__(self):
-        return self.df.to_markdown()
-
-    def __len__(self):
+    def __len__(self) -> int:
         if len(self.df) > 0:
             return len(self.df)
         else:
             return 0
 
-    def get_code(self, index: list):
+    def get_code(self, row_numbers: list) -> list:
         """
-        Returns the for a given list of tables
+        Returns the code for a given list of tables.
 
         Args:
-             index (list): A list that contains the indices from the results objects. This is not the object code."
+             row_numbers (list): A list that contains the row numbers from the results objects. This is not the object code."
         Returns:
              table codes (list): Contains the corresponding tables codes.
         """
 
-        codes = self.df.iloc[index]["Code"]
+        codes = self.df.iloc[row_numbers]["Code"]
         return list(codes)
 
-    def get_metadata(self, index: list):
+    def get_metadata(self, row_numbers: list) -> None:
         """
         Prints meta data for a given list of tables.
 
         Args:
-              index (list): A list that contains the indices from the results objects. This is not the object code."
-        Returns:
-              prints meta data for all indices.
+              row_numbers (list): A list that contains the row_numbers from the results objects. This is not the object code."
         """
-        codes = self.df.iloc[index]["Code"]
+        codes = self.df.iloc[row_numbers]["Code"]
 
-        for code, ix in zip(codes, index):
-            response = self._get_metadata_results(self.category[0:-1], code)
+        for code, ix in zip(codes, row_numbers):
+            response = self._get_metadata_results(
+                self.category[0:-1], code
+            )  # Category is truncated, because
+            # metadata endpoints works with singulars
 
             if self.category == "tables":
                 structure_dict = response["Object"]["Structure"]
@@ -165,8 +168,10 @@ class Results:
             "name": code,
         }
 
-        response = get_response_from_endpoint("metadata", category, params)
-        response_json = response.json()
+        response = get_data_from_endpoint(
+            endpoint="metadata", method=category, params=params
+        )
+        response_json = json.loads(response)
 
         return response_json
 
@@ -177,14 +182,9 @@ class Find:
 
     Attributes:
         query (str): The query that is provided to find endpoint.
-        top_n_preview (int): Number of previews in print summary.
-
-    Methods:
-        _print_summary(): Prints a summary with result previews.
-        _get_find_results(): Gets results from the find endpoints.
     """
 
-    def __init__(self, query: str, top_n_preview: int = 5) -> None:
+    def __init__(self, query: str) -> None:
         """Method for retrieving data from find endpoint.
 
         Args:
@@ -199,71 +199,75 @@ class Find:
         """
 
         self.query = query
+        self.top_n_preview = None
+        self.statistics = None
+        self.variables = None
+        self.tables = None
+        self.cubes = None
+        self.is_run = False
+
+    def run(self, top_n_preview: int = 5):
         self.top_n_preview = top_n_preview
-        self.statistics = self._get_find_results(query, "statistics")
-        self.variables = self._get_find_results(query, "variables")
-        self.tables = self._get_find_results(query, "tables")
-        self.cubes = self._get_find_results(query, "cubes")
+        self.statistics = self._get_find_results("statistics")
+        self.variables = self._get_find_results("variables")
+        self.tables = self._get_find_results("tables")
+        self.cubes = self._get_find_results("cubes")
 
-        print(self._print_summary())
+        self.is_run = True
 
-    def _print_summary(self):
-        return "\n".join(
-            [
-                "##### Results #####",
-                f"{'-' * 40}",
-                f"# Number of tables: {len(self.tables.df)}",
-                "# Preview:",
-                self.tables.df.iloc[0 : self.top_n_preview].to_markdown(),
-                f"{'-' * 40}"
-                f"# Number of statistics: {len(self.statistics.df)}",
-                "# Preview:",
-                self.statistics.df.iloc[0 : self.top_n_preview].to_markdown(),
-                f"{'-' * 40}",
-                f"# Number of variables: {len(self.variables.df)}",
-                "# Preview:",
-                self.variables.df.iloc[0 : self.top_n_preview].to_markdown(),
-                f"{'-' * 40}",
-                f"# Number of cubes: {len(self.cubes.df)}",
-                "# Preview:",
-                self.cubes.df.iloc[0 : self.top_n_preview].to_markdown(),
-                f"{'-' * 40}",
-                "# Info: Use object.tables, object.statistics, object.variables or object.cubes to get all results.",
-                f"{'-' * 40}",
-            ]
-        )
+        print(self.summary())
 
-    @staticmethod
-    def _get_find_results(query: str, category: str, **kwargs) -> Results:
+    def summary(self):
+        if self.is_run:
+            return "\n".join(
+                [
+                    "##### Results #####",
+                    f"{'-' * 40}",
+                    f"# Number of tables: {len(self.tables.df)}",
+                    "# Preview:",
+                    self.tables.df.iloc[: self.top_n_preview].to_markdown(),
+                    f"{'-' * 40}"
+                    f"# Number of statistics: {len(self.statistics.df)}",
+                    "# Preview:",
+                    self.statistics.df.iloc[: self.top_n_preview].to_markdown(),
+                    f"{'-' * 40}",
+                    f"# Number of variables: {len(self.variables.df)}",
+                    "# Preview:",
+                    self.variables.df.iloc[: self.top_n_preview].to_markdown(),
+                    f"{'-' * 40}",
+                    f"# Number of cubes: {len(self.cubes.df)}",
+                    "# Preview:",
+                    self.cubes.df.iloc[: self.top_n_preview].to_markdown(),
+                    f"{'-' * 40}",
+                    "# Info: Use object.tables, object.statistics, object.variables or object.cubes to get all results.",
+                    f"{'-' * 40}",
+                ]
+            )
+        else:
+            return "No data found. Please use .run() to retrieve data."
+
+    def _get_find_results(self, category: str, **kwargs) -> Results:
         """
         Based on the query (term), category and additional query parameters a Result object will be created.
 
         Args:
-            query (str): Search term.
             category (str): Category of the result. E.g. "tables", "cubes"
             query_params (dict, optional): Additional query parameters (Default: None)
         Returns:
             pd.DataFrame
         """
 
-        kwargs = kwargs or {}
-
         params = {
-            "term": query,
+            "term": self.query,
             "category": category,
         }
 
         params |= kwargs
 
-        response = get_response_from_endpoint("find", "find", params)
-        response_json = response.json()[category.capitalize()]
-        response_df = pd.DataFrame(response_json).replace("\n", " ", regex=True)
+        response = get_data_from_endpoint(
+            endpoint="find", method="find", params=params
+        )
+        response_dict = json.loads(response)[category.capitalize()]
+        response_df = pd.DataFrame(response_dict).replace("\n", " ", regex=True)
 
         return Results(response_df, category)
-
-
-if __name__ == "__main__":
-    results = Find("Rohöl")
-    results.tables.get_code([1, 2, 3])
-    results.tables.get_metadata([1, 2])
-    results.cubes.get_metadata([1, 2])
