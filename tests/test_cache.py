2c8d12fb5bffa51457ba9a47baaacb1b8fafdff6
Michael Aydinbas <michael.aydinbas@gmail.com>
2022-09-27 21:46:49 +0200

    Feat/improve cache (#71)
    * refactor cache to work properly for jobs: no longer cache endpoint and method but only name as identifier, so a cache hit happens when the unique name is found in the cache data, irrelevant of the endpoint or method, thus working for data/resultfile and data/tablefile
    
    * normalize an object name so we can strip off the job-id
    
    * fix cicd
    
    * fix cicd
    
    * fix isort settings
    
    * fix mypy issues
    
    * add comments

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -9,6 +9,7 @@ from pygenesis.cache import (
     cache_data,
     clear_cache,
     hit_in_cash,
+    normalize_name,
     read_from_cache,
 )
 from pygenesis.config import (
@@ -18,8 +19,6 @@ from pygenesis.config import (
     load_config,
     load_settings,
 )
-
-SLEEP_TIME = 0.1
 
 
 @pytest.fixture()
@@ -49,10 +48,10 @@ def params():
 
 
 def test_build_file_path(cache_dir, params):
-    data_dir = _build_file_path(cache_dir, "test", "data", "test-cache", params)
+    data_dir = _build_file_path(cache_dir, "test-build-file-path", params)
 
     assert isinstance(data_dir, Path)
-    assert data_dir.parent == Path(cache_dir) / "test" / "data" / "test-cache"
+    assert data_dir.parent == Path(cache_dir) / "test-build-file-path"
     assert data_dir.name.isalnum()
 
 
@@ -60,11 +59,9 @@ def test_cache_data(cache_dir, params):
     assert len(list((cache_dir / "data").glob("*"))) == 0
 
     test_data = "test"
-    cache_data(cache_dir, "test-cache", "data", "test-cache", params, test_data)
+    cache_data(cache_dir, "test-cache-data", params, test_data)
 
-    data_dir = _build_file_path(
-        cache_dir, "test-cache", "data", "test-cache", params
-    )
+    data_dir = _build_file_path(cache_dir, "test-cache-data", params)
 
     assert data_dir.exists() and len(list(data_dir.glob("*.zip"))) == 1
 
@@ -74,47 +71,55 @@ def test_read_from_cache(cache_dir, params):
     cache_data(
         cache_dir,
         "test-read-cache",
-        "data",
-        "test-read-cache",
         params,
         test_data,
     )
-    data = read_from_cache(
-        cache_dir, "test-read-cache", "data", "test-read-cache", params
-    )
+    data = read_from_cache(cache_dir, "test-read-cache", params)
 
     assert data == test_data
 
 
 def test_hit_cache(cache_dir, params):
-    assert not hit_in_cash(
-        cache_dir, "test-hit-cache", "data", "test-hit-cache", params
-    )
-    cache_data(
-        cache_dir, "test-hit-cache", "data", "test-hit-cache", params, "test"
-    )
-    assert hit_in_cash(
-        cache_dir, "test-hit-cache", "data", "test-hit-cache", params
-    )
+    assert not hit_in_cash(cache_dir, "test-hit-cache", params)
+    cache_data(cache_dir, "test-hit-cache", params, "test")
+    assert hit_in_cash(cache_dir, "test-hit-cache", params)
+
+
+def test_normalize_name():
+    full_name = "42153-0001_878150652"
+    assert normalize_name(full_name) == "42153-0001"
 
 
 def test_change_in_params(cache_dir, params):
     params_ = params.copy()
 
     name = "test-change-in-params"
-    assert not hit_in_cash(cache_dir, name, "data", name, params_)
-    cache_data(cache_dir, name, "data", name, params_, "test")
-    assert hit_in_cash(cache_dir, name, "data", name, params_)
+    assert not hit_in_cash(cache_dir, name, params_)
+    cache_data(cache_dir, name, params_, "test")
+    assert hit_in_cash(cache_dir, name, params_)
 
     params_.update({"new-param": 2})
-    assert not hit_in_cash(cache_dir, name, "data", name, params_)
+    assert not hit_in_cash(cache_dir, name, params_)
+
+
+def test_ignore_jobs_in_params(cache_dir, params):
+    params_ = params.copy()
+
+    name = "test-ignore-jobs"
+    cache_data(cache_dir, name, params_, "test")
+
+    params_.update({"job": True})
+    assert hit_in_cash(cache_dir, name, params_)
+
+    params_.update({"job": False})
+    assert hit_in_cash(cache_dir, name, params_)
 
 
 def test_clean_cache(cache_dir, params):
     name = "test-clean-cache"
-    cache_data(cache_dir, name, "data", name, params, "test")
+    cache_data(cache_dir, name, params, "test")
 
-    data_dir = _build_file_path(cache_dir, name, "data", name, params)
+    data_dir = _build_file_path(cache_dir, name, params)
     cached_data_file = list(data_dir.glob("*.zip"))[0]
 
     assert cached_data_file.exists() and cached_data_file.is_file()
